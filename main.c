828d0cb83e2dceb4ebc33c5a508d8fc5d3435a01
Reece Martin <capt.rm.fae@gmail.com>
2018-10-24 22:28:13 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int  main ( int argc, char **argv ) {
+     
+  // Process the inputs
+  
+  // Generate the array  
+  
+  // Start Timing
+  
+  // Multiply Linear
+  
+  // Stop Timing
+  
+  // Start Timing
+  
+  // Multiply Parallel
+  
+  // Stop Timing
+  
+  // Print Results
+  
+     return 0;
+}
