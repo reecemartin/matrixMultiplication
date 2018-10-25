2249a86fec61461d589bccb90ac7ebcfbcb51b13
Reece Martin <capt.rm.fae@gmail.com>
2018-10-24 22:25:37 -0400

    Update matrix_multiply_linear.c

--- matrix_multiply_linear.c
+++ matrix_multiply_linear.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 #include <math.h>
 
-int[][] linear_multiply(int[][] matrix_1, int[][] matrix_2){
+int*** linear_multiply(int[][] matrix_1, int[][] matrix_2, int*** result_space){
   
 }
